0573435dac7ed34a4f48ee0b388bba0b8b9bce8b
Jose Valdez <josevaldez@pop-os.localdomain>
2022-11-30 20:30:11 -0700

    First commit

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+export const SITE = {
+    // Your site's detail?
+    name: 'Ink',
+    title: 'Jose Valdez Gaxiola',
+    description: 'Crisp, minimal, personal blog theme for Astro',
+    image: '/profile.jpg',
+    githubUrl: 'https://github.com/one-aalam/astro-ink',
+   
+    
+}
