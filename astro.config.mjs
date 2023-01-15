3315b4a20c89d3598fab3a22345d60497f0ef37d
Jose Valdez Gaxiola <josevaldezgaxiola@gmail.com>
2023-01-14 18:46:09 -0700

    Deploy to github

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  site: "https://josevaldezgaxiola.github.io",
 
   server: { port: 5000, host: true },
-});
+})
