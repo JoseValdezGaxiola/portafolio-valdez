503f239571c7fe4f84310a06c7b05058ced80e62
Jose Valdez Gaxiola <josevaldezgaxiola@gmail.com>
2023-01-14 17:54:14 -0700

    Improvements and new components

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,82 @@
 export const SITE = {
-    // Your site's detail?
-    name: 'Ink',
-    title: 'Jose Valdez Gaxiola',
-    description: 'Crisp, minimal, personal blog theme for Astro',
-    image: '/profile.jpg',
-    githubUrl: 'https://github.com/one-aalam/astro-ink',
-   
-    
-}
+  name: "Jose Valdez",
+  title: "Jose Valdez Gaxiola",
+  description: "Portafolio",
+  githubUrl: "https://github.com/JoseValdezGaxiola",
+};
+
+export const ICONS = {
+  data: [
+    {
+      icon: "simple-icons:javascript",
+      toolpit: "JAVASCRIPT",
+    },
+    {
+      icon: "simple-icons:react",
+      toolpit: "REACTJS",
+    },
+    {
+      icon: "simple-icons:tailwindcss",
+      toolpit: "TAILWINDCSS",
+    },
+    {
+      icon: "simple-icons:nextdotjs",
+      toolpit: "NEXT",
+    },
+    {
+      icon: "simple-icons:css3",
+      toolpit: "CSS3",
+    },
+
+    {
+      icon: "simple-icons:astro",
+      toolpit: "ASTRO",
+    },
+
+    {
+      icon: "simple-icons:html5",
+      toolpit: "HTML5",
+    },
+    {
+      icon: "simple-icons:github",
+      toolpit: "GITHUB",
+    },
+  ],
+};
+
+export const PROJECTS = {
+  info: [
+    {
+      description:
+        "Inspired by crypto.com/price, cryptocurrency price tracker  built with React and powered by the Coingecko API. ",
+      github: "https://github.com/JoseValdezGaxiola/cryptomarket",
+      website: "https://cryptomarket-project.netlify.app/",
+      image: "/cryptomarket.avif",
+    },
+    {
+      description:
+        "Proof of concept for a gym coach landing page built with AstroJs and Tailwind",
+      github: "https://github.com/JoseValdezGaxiola/landig-gym",
+      website: "https://landing-gym-project.netlify.app/",
+      image: "/gyml.avif",
+    },
+
+    {
+      description: "Personal portfolio built with astro js",
+      github: "https://github.com/JoseValdezGaxiola/landig-gym",
+      website: "https://landing-gym-project.netlify.app/",
+      image: "/Jose-Valdez-Gaxiola.avif",
+    },
+  ],
+};
+
+export const ABOUT = {
+  data: [
+    {
+      para: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem soluta, odit officiis consequuntur delectus iusto repudiandae vitae nemo sit molestias optio inventore saepe labore modi, tempore quo consequatur ex sunt!",
+    },
+    {
+      para: " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem soluta, odit officiis consequuntur delectus iusto repudiandae vitae nemo sit molestias optio inventore saepe labore modi, tempore quo consequatur ex sunt! officiis consequuntur delectus iusto repudiandae vitae nemo sit molestias optio inventore saepe labore modi, tempore quo consequatur ex sunt!",
+    },
+  ],
+};
