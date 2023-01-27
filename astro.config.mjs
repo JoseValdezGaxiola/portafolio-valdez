4b7d2c309c3d3b23aa33dab6691aa03bf61de9e3
Jose Valdez Gaxiola <josevaldezgaxiola@gmail.com>
2023-01-27 12:27:52 -0700

    fix: correct branch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,15 @@
 import { defineConfig } from "astro/config"
-<<<<<<< HEAD
+
 import astroI18next from "astro-i18next"
-=======
->>>>>>> main
+
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
-<<<<<<< HEAD
+
   integrations: [tailwind(), astroI18next()],
-=======
-  integrations: [tailwind()],
-  site: "https://josevaldezgaxiola.github.io",
->>>>>>> main
 
   server: { port: 5000, host: true },
 })
